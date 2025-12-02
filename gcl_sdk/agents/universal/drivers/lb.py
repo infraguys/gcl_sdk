3882f2bb9d9e3ec89c9f68bd35049bbcaa2bf70b
George Melikov <mail@gmelikov.ru>
2025-12-02 18:27:31 +0300

    Introduce LBCapabilityDriver

--- gcl_sdk/agents/universal/drivers/lb.py
+++ gcl_sdk/agents/universal/drivers/lb.py
@@ -23,53 +23,15 @@ from gcl_sdk.agents.universal.drivers import exceptions as driver_exc
 from gcl_sdk.agents.universal.drivers import meta
 from gcl_sdk.agents.universal import constants as c
 from gcl_sdk.infra import constants as ic
-from gcl_sdk.paas.dm import services as s_models
+from gcl_sdk.paas.dm import lb as lb_models
 
 
 LOG = logging.getLogger(__name__)
-SERVICE_TARGET_KIND = "service_agent_node"
-SERVICES_DIR = "/etc/systemd/system/"
-NAME_PREFIX = "genesis_srv"
 
-SERVICE_TEMPLATE = """\
-[Unit]
-Description=Genesis Core: dynamic service {name}
-After=network.target
-
-[Service]
-Type={service_type}
-Restart={restart}
-ExecStart=/usr/bin/bash -c '{command}'
-User={user}
-{group}
-{pre}
-{post}
-RestartSec=5s
-TimeoutStopSec=30
-
-[Install]
-WantedBy=multi-user.target
-"""
-
-TYPES_MAPPING = {
-    "simple": "simple",
-    "oneshot": "oneshot",
-    "monopoly": "simple",
-    "monopoly_oneshot": "oneshot",
-}
-CONDITIONS_MAPPING = {
-    "before": {"service": "After", "shell": "ExecStartPre"},
-    "after": {"service": "Before", "shell": "ExecStartPost"},
-}
+LB_TARGET_KIND = "paas_lb_node"
 
 
-class Service(s_models.Service, meta.MetaDataPlaneModel):
-
-    def get_my_name(self):
-        return self.get_service_name(self.name, self.uuid)
-
-    def get_service_name(self, name, uuid):
-        return f"{NAME_PREFIX}_{name}_{uuid}.service"
+class LB(lb_models.LB, meta.MetaDataPlaneModel):
 
     def get_meta_model_fields(self) -> set[str] | None:
         """Return a list of meta fields or None.
@@ -84,45 +46,12 @@ class Service(s_models.Service, meta.MetaDataPlaneModel):
         # Keep all fields as meta fields for simplicity
         return {
             "uuid",
-            "name",
-            "path",
-            "user",
-            "group",
-            "service_type",
-            "before",
-            "target_status",
-            "after",
+            "vhosts",
+            "backends",
         }
 
-    def _parse_conditions(self, conditions, when):
-        res = []
-        for cond in conditions:
-            if cond.kind == "service":
-                # Disabled, service relationships should be reworked
-                # res.append(f"{CONDITIONS_MAPPING[when]["service"]}={self.get_service_name(cond.service, cond.service_name)}")
-                continue
-            res.append(
-                f"{CONDITIONS_MAPPING[when]["shell"]}=+/usr/bin/bash -c '{cond.command.replace("'", "\\'")}'"
-            )
-        return res
-
-    def _gen_file_content(self):
-        return SERVICE_TEMPLATE.format(
-            name=self.name,
-            service_type=TYPES_MAPPING[self.service_type.kind],
-            restart=(
-                "always"
-                if not self.service_type.kind.endswith("oneshot")
-                else "on-failure"
-            ),
-            command=self.path.replace("'", "\\'"),
-            user=self.user or "root",
-            group=f"Group={self.group}" if self.group else "",
-            pre="\n".join(self._parse_conditions(self.before, "before")),
-            post="\n".join(self._parse_conditions(self.after, "after")),
-        )
-
     def dump_to_dp(self) -> None:
+        return
         with open(f"{SERVICES_DIR}{self.get_my_name()}", "w") as f:
             f.write(self._gen_file_content())
 
@@ -138,6 +67,7 @@ class Service(s_models.Service, meta.MetaDataPlaneModel):
         self.status = ic.InstanceStatus.ACTIVE.value
 
     def restore_from_dp(self) -> None:
+        return
         try:
             subprocess.check_output(
                 ["systemctl", "is-active", self.get_my_name()]
@@ -166,6 +96,7 @@ class Service(s_models.Service, meta.MetaDataPlaneModel):
             )
 
     def delete_from_dp(self) -> None:
+        return
         subprocess.check_call(
             ["systemctl", "disable", "--now", self.get_my_name()]
         )
@@ -176,16 +107,17 @@ class Service(s_models.Service, meta.MetaDataPlaneModel):
         subprocess.check_call(["systemctl", "daemon-reload"])
 
     def update_on_dp(self) -> None:
+        return
         """Update the resource on the data plane."""
         # The simplest implementation, just recreate.
         self.delete_from_dp()
         self.dump_to_dp()
 
 
-class ServiceCapabilityDriver(meta.MetaFileStorageAgentDriver):
-    SERVICE_META_PATH = os.path.join(c.WORK_DIR, "service_meta.json")
+class LBCapabilityDriver(meta.MetaFileStorageAgentDriver):
+    META_PATH = os.path.join(c.WORK_DIR, "lb_meta.json")
 
-    __model_map__ = {SERVICE_TARGET_KIND: Service}
+    __model_map__ = {LB_TARGET_KIND: LB}
 
     def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, meta_file=self.SERVICE_META_PATH, **kwargs)
+        super().__init__(*args, meta_file=self.META_PATH, **kwargs)
